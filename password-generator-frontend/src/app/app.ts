6069b109bde1e5c939a35d692dfa65c9f97bc010
SofianLabrak <sofian.labrak@gmail.com>
2025-10-17 14:17:29 +0200

    Ajouter d'un component Footer et d'un style Footer

--- password-generator-frontend/src/app/app.ts
+++ password-generator-frontend/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Button} from 'primeng/button';
 import { Header } from './components/header/header';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
   standalone: true,
   styleUrl: './app.scss'
