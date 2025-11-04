09ba10c338c4d950f7b9d695968151095d5fbf64
SofianLabrak <sofian.labrak@gmail.com>
2025-11-04 11:25:37 +0100

    Ajout d'un slider avec la logique en Front afin de généré un mot de passe aléatoirement selon la position du slider.

--- password-generator-frontend/src/app/components/page-generation-mdp/page-generation-mdp.ts
+++ password-generator-frontend/src/app/components/page-generation-mdp/page-generation-mdp.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import {Checkbox} from 'primeng/checkbox';
+import { SliderModule } from 'primeng/slider';
 
 @Component({
   selector: 'app-page-generation-mdp',
-  imports: [ButtonModule, Checkbox],
+  imports: [ButtonModule, Checkbox, SliderModule, FormsModule],
   templateUrl: './page-generation-mdp.html',
   standalone: true,
   styleUrl: './page-generation-mdp.scss'
 })
 export class PageGenerationMdp {
+  lengthMdp: number = 16;
+  generatedPassword: string = '';
+
+  constructor() {
+    this.generatePassword();
+  }
+
+  generatePassword(): void {
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[]{}|;:,.<>?';
+    let password = '';
+    for (let i = 0; i < this.lengthMdp; i++) {
+      password += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    this.generatedPassword = password;
+  }
+
+  onSliderChange(): void {
+    this.generatePassword();
+  }
 }
